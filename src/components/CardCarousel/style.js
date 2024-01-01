943844a7b3c584abb11e52ff3c617cb073217ce3
Samuel Pereira Muleu <samuelpereiramuleu@gmail.com>
2024-01-01 12:04:46 -0300

    page home

--- src/components/CardCarousel/style.js
+++ src/components/CardCarousel/style.js
@@ -4,13 +4,14 @@ import { COLORS_THEME } from "../../styles/theme";
 const Container = styled.div`
 
 display: flex;
-align-items: center;
 padding: 20px;
-height: 350px;
+gap: 16px;
 color:${COLORS_THEME.LIGHT_100} ;
 >div{
-border: 3px solid ${COLORS_THEME.DARK_100};
-height: 330px;
+border: 3px solid ${COLORS_THEME.DARK_300};
+background-color: ${COLORS_THEME.DARK_200};
+border-radius: 18px;
+
 
 >button{
    
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -5,7 +5,7 @@ import { COLORS_THEME } from "../../styles/theme";
 const Container = styled.div`
 margin-top: auto;
 
-width: 100%;
+width: 100vw;
 height: 77px;
 background-color: ${COLORS_THEME.DARK_600} ;
 
