a0025988e508eccc2fad074d28b8467dcb7629a9
Samuel Pereira Muleu <samuelpereiramuleu@gmail.com>
2024-01-01 12:48:33 -0300

    config on deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-
 import {register} from "swiper/element/bundle";
-
 
 register();
 import "swiper/css";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-
-
-
 
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
