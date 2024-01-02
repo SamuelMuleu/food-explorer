28e988d088fd6883df461f0dcbeece00915035e9
Samuel Pereira Muleu <samuelpereiramuleu@gmail.com>
2024-01-01 22:24:56 -0300

    resolve routes

--- src/components/CarouselComponent/index.jsx
+++ src/components/CarouselComponent/index.jsx
@@ -1,10 +1,4 @@
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Virtual,
-} from "swiper/modules";
+import { Navigation, Pagination,Scrollbar, A11y,Virtual,} from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -12,8 +6,8 @@ import "swiper/css/bundle";
 import CardCarousel from "../../components/CardCarousel";
 
 const CarouselComponent = () => {
-  const slides = Array.from({ length: 1000 }).map(
-    ( id,index) => `Slide ${index + 1}`
+  const slides = Array.from({ length: 10 }).map(
+    (index) => `Slide ${index + 1}`
   );
   return (
     <Swiper
@@ -24,8 +18,8 @@ const CarouselComponent = () => {
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
     >
-      {slides.map((slideContent) => (
-        <SwiperSlide key={slideContent.id}>
+      {slides.map((index) => (
+        <SwiperSlide key={index}>
           <CardCarousel />
         </SwiperSlide>
       ))}
